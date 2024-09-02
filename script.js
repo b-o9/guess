ab5c439b19e9203b2c2328fb052da9a377a69154
SS4nd <silas.sandager@gmail.com>
2024-09-02 17:05:15 +0200

    Implemeted binary search

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-let guess = 0;
-let sidsteGuess = [];
+let guess = 50;
+let listen = Array.from({ length: 100 }, (_, i) => i + 1);
+let floor = 1;
+let ceil = 100;
+let guesses = 0;
 
-function backendGuess(win) {
-    if (win){
+document.getElementById('lowBtn').addEventListener('click', () => guessHigher());
+document.getElementById('correctBtn').addEventListener('click', () => win());
+document.getElementById('highBtn').addEventListener('click', () => guessLower());
+
+function guessLower() {
+    guesses++;
+    ceil = guess - 1;
+    listen = listen.filter(num => num <= guess-1);
+    updateList();
+
+    guess = Math.floor((ceil - floor) / 2) + floor;
+    document.getElementById("guess").innerText = "Is it " + guess + "?";
+}
+
+function guessHigher() {
+    guesses++;
+    floor = guess + 1;
+    listen = listen.filter(num => num >= guess+1);
+    updateList();
+
+    guess = Math.floor((ceil - floor) / 2) + floor;
+    document.getElementById("guess").innerText = "Is it " + guess + "?";
+}
+
+function win() {
+    guesses++;
+    document.getElementById("guesses").innerText = `Guesses: ${guesses}`;
+    document.getElementById("guessList").innerText = `Update the site to play again`;
+    if (guesses < 5){
         document.getElementById("guess").innerText = "Good game";
-        exit()
+    } else {
+        document.getElementById("guess").innerText = "Bad game";
     }
-    if (sidsteGuess !== null) {
-        document.getElementById("sidsteGuess").innerText = "Previous number: " + sidsteGuess;
-    }
-    guess = Math.floor(Math.random() * 100) + 1;
-    sidsteGuess.push(guess);
-    document.getElementById("guess").innerText = "Is it "+guess+"?";
+}
 
+function updateList (){
+    document.getElementById("guesses").innerText = `Guesses: ${guesses}`;
+    if (listen == "undefined"){
+        document.getElementById("guessList").innerText = `You are cheating...`;
+    } else {
+        document.getElementById("guessList").innerText = `Range: ${listen[0]} - ${listen[listen.length - 1]}`;
+    }
 }
